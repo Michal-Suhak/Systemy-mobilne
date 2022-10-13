ce8c992fb6ce27311539bb0c2b133f504c2c87d2
michal-suhak <michal.suhak@gmail.com>
2022-10-13 17:23:28 +0200

    delete overrides on the quiz app

--- Quiz_app/app/src/main/java/com/example/quiz_app/MainActivity.java
+++ Quiz_app/app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -1,28 +1,18 @@
 package com.example.quiz_app;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private String QUIZ_TAG = "quiz_tag";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
-    private Button promptButton;
     private TextView questionTextView;
-
-    private static boolean answerWasShown;
-    private static final int REQUEST_CODE_PROMPT = 0;
-    private static final String KEY_CURRENT_INDEX = "currentIndex";
-    public static final String KEY_EXTRA_ANSWER = "com.example.quiz_app.answerShown";
 
     private Question[] questions = new Question[]{
             new Question(R.string.q_activity, true),
@@ -35,49 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private int currentIndex = 0;
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(QUIZ_TAG, "Pierwsze uruchomienie");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(QUIZ_TAG,"Ponownie uruchomiono aplikacje");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(QUIZ_TAG, "Zatrzymanon aplikacje");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(QUIZ_TAG, "Wyłączono aplikacje");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(QUIZ_TAG, "Zniszono aplikacje");
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState != null) {
-            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
-        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
-        promptButton = findViewById(R.id.tip_button);
         questionTextView = findViewById(R.id.question_text_view);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
@@ -101,59 +55,23 @@ public class MainActivity extends AppCompatActivity {
                 setNextQuestion();
             }
         });
-
-        promptButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
-                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
-                intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
-                startActivityForResult(intent, REQUEST_CODE_PROMPT);
-            }
-        });
-
         setNextQuestion();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) {
-            return;
-        }
-        if (requestCode == REQUEST_CODE_PROMPT) {
-            if (data == null) {
-                return;
-            }
-            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
-        }
     }
 
     private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if (answerWasShown) {
-            resultMessageId = R.string.answer_was_shown;
+        if (userAnswer == correctAnswer) {
+            resultMessageId = R.string.correct_answer;
         }
         else {
-            if (userAnswer == correctAnswer) {
-                resultMessageId = R.string.correct_answer;
-            } else {
-                resultMessageId = R.string.incorrect_answer;
-            }
-            Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+            resultMessageId = R.string.incorrect_answer;
         }
         Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
 
     private void setNextQuestion() {
         questionTextView.setText(questions[currentIndex].getQuestionId());
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState");
-        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
     }
 }
 
